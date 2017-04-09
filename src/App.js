9064fc84ab816b1e3276cf7d1e4ffd698044d08b
humbl3man <konstantindev925@gmail.com>
2017-04-09 12:25:39 -0700

    add global alert. minor styling updates to nav

--- src/App.js
+++ src/App.js
@@ -4,10 +4,28 @@ import Footer from './components/Footer';
 import "./styles/site.css";
 import "./styles/page.css";
 
+const Alert = () => {
+    return (
+        <div className="alert alert-warning alert-dismissible fade show global-alert" role="alert">
+            <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            <strong>Heads up!</strong> This site is a work in progress. I will be adding more content soon.
+        </div>
+    );
+};
+
 export default class App extends Component {
+    constructor() {
+        super();
+        this.showGlobalAlert = true;
+    }
+
     render() {
         return (
             <div id="site">
+                {this.showGlobalAlert &&
+                <Alert />}
                 <div id="content">
                     {this.props.children}
                 </div>
