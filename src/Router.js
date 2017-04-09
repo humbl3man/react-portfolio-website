32b3fa7da9f5eb10289790a174b8a1e0d8078dff
humbl3man <konstantindev925@gmail.com>
2017-04-08 18:20:30 -0700

    add hash routing

--- src/Router.js
+++ src/Router.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import {BrowserRouter as Router, Route } from 'react-router-dom';
+import {HashRouter, Route } from 'react-router-dom';
 import App from './App';
 import Home from './components/Home';
 import Bio from './components/Bio';
 import Work from './components/Work';
 
 const routes = (
-  <Router>
+  <HashRouter>
     <App>
       <Route exact path="/" component={Home} />
       <Route path="/bio" component={Bio} />
       <Route path="/work" component={Work} />
     </App>
-  </Router>
+  </HashRouter>
 );
 
 export default routes;
