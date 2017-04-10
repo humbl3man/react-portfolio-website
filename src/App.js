a947adcd8a45882e49ba4c8a8f6f403b1c2b4a55
humbl3man <konstantindev925@gmail.com>
2017-04-09 22:00:22 -0700

    keep notification dismiss status in browser storage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, {Component} from 'react';
+
+
+
 import Footer from './components/Footer';
 
 import "./styles/site.css";
 import "./styles/page.css";
 
-const Alert = () => {
+const Notification = (props) => {
+    function rememberDismiss() {
+        props.rememberDismiss();
+    }
+
     return (
         <div className="alert alert-warning alert-dismissible fade show global-alert" role="alert">
-            <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+            <button onClick={rememberDismiss} type="button" className="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
             <strong>Heads up!</strong> This site is a work in progress. I will be adding more content soon.
@@ -18,14 +25,20 @@ const Alert = () => {
 export default class App extends Component {
     constructor() {
         super();
-        this.showGlobalAlert = true;
+        this.showGlobalAlert = !JSON.parse(window.localStorage.getItem('dismissAlert')) ? true : false;
+    }
+
+    rememberDismiss = () => {
+        if (window.localStorage) {
+            window.localStorage.setItem('dismissAlert', true);
+        }
     }
 
     render() {
         return (
             <div id="site">
                 {this.showGlobalAlert &&
-                <Alert />}
+                <Notification rememberDismiss={this.rememberDismiss}/>}
                 <div id="content">
                     {this.props.children}
                 </div>
